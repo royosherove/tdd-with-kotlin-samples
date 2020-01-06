6d68235369fe72e560fa595987f942938a98f1b8
royo <royo>
2020-01-07 01:42:24 +0200

    mockkStatic with Calendar

--- src/test/kotlin/withJunit5/dynamicFakes/StringCalcWithTimeTests.kt
+++ src/test/kotlin/withJunit5/dynamicFakes/StringCalcWithTimeTests.kt
@@ -1,51 +1,65 @@
-//package withJunit5.manualFakes
-//import org.assertj.core.api.Assertions.*
-//import org.junit.jupiter.api.Assertions.assertEquals
-//import org.junit.jupiter.api.Test
-//import underTest.StringCalcWithTime
-//import java.util.*
-//
-//class TestableStringCalcWithTime(val currentDayWillBe: Int) : StringCalcWithTime(){
-//
-//    override fun getCurrentDay(): Int {
-//        return currentDayWillBe
-//    }
-//
-//}
-//
-//class StringCalcWithTimeTests {
-//    fun makeCalc(day:Int): TestableStringCalcWithTime {
-//        return TestableStringCalcWithTime(day)
-//    }
-//
-//    fun makeEnabledCalc(): TestableStringCalcWithTime {
-//        return makeCalc(Calendar.MONDAY)
-//    }
-//
-//    @Test
-//    fun `adding a number on SUNDAYS returns ignored result`(){
-//        val sc = makeCalc(Calendar.SUNDAY)
-//
-//        val result = sc.add("")
-//
-//        assertThat(result).isEqualTo(-1)
-//    }
-//
-//    @Test
-//    fun `Add with empty string returns default`(){
-//        val sc = makeEnabledCalc()
-//
-//        val result = sc.add("")
-//
-//        assertEquals(0,result)
-//    }
-//
-//    @Test
-//    fun `with single number returns that number`(){
-//        val sc = makeEnabledCalc()
-//
-//        val result = sc.add("1")
-//
-//        assertEquals(1, result)
-//    }
-//}
+package withJunit5.dynamicFakes
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import underTest.StringCalcWithTime
+import java.util.*
+
+
+class StringCalcWithTimeTests {
+    object Helpers{
+
+        fun makeCalc(): StringCalcWithTime {
+            return StringCalcWithTime()
+        }
+
+        fun fakeStaticCalendarDayOfWeek(day: Int) {
+            mockkStatic(Calendar::class)
+            every { Calendar.getInstance().get(Calendar.DAY_OF_WEEK) } returns day
+        }
+
+        fun makeCalcWithEnabledDateDepenendecy(): StringCalcWithTime {
+            fakeStaticCalendarDayOfWeek(Calendar.MONDAY)
+            return makeCalc()
+        }
+
+    }
+    @AfterEach
+    fun `teardown`(){
+        unmockkAll()
+    }
+
+    @Test
+    fun `adding a number on SUNDAYS returns ignored result`(){
+        Helpers.fakeStaticCalendarDayOfWeek(Calendar.SUNDAY)
+        val sc = Helpers.makeCalc()
+
+        val result = sc.add("")
+
+        assertThat(result).isEqualTo(-1)
+    }
+
+
+    @Test
+    fun `Add with empty string returns default`(){
+        val sc = Helpers.makeCalcWithEnabledDateDepenendecy()
+
+        val result = sc.add("")
+
+        assertEquals(0,result)
+    }
+
+
+    @Test
+    fun `with single number returns that number`(){
+        val sc = Helpers.makeCalcWithEnabledDateDepenendecy()
+
+        val result = sc.add("1")
+
+        assertEquals(1, result)
+    }
+}
